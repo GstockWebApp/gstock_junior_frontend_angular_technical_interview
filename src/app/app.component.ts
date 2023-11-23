f6f6291e7d31b515a8933039aaed59d65506d8ac
Saul <saul.fernandez@gstock.app>
2023-11-23 11:48:00 +0100

    chore: initial commit with angular 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'prueba-tecnica';
